8e6d1a7e86d96d3bac62ece599342c980ee2554b
Fmkwebu <fikile6666@gmail.com>
2023-09-08 06:30:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
-* main - this is the main put function
-* Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
