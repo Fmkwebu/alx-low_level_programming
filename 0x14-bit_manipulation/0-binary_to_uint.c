5ebcc328d5a6b470cedd0260f48fc0f074639d38
Fmkwebu <fikile6666@gmail.com>
2023-10-26 09:21:12 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * binary_to_uint
+ * binary_to_uint- binary can be converted
  * @b: A string comprise of binary numbers
  * Return: converted number of 0
  */
@@ -16,9 +16,9 @@ unsigned int binary_to_uint(const char *b)
 	return (0);
 	for (zip = 0; b[zip]; zip++)
 	{
-	if (b[alx] < '0' || b[alx] > '1')
+	if (b[zip] < '0' || b[zip] > '1')
 	return (0);
-	mello = 2 * mello + (b[alx] - '0');
+	mello = 2 * mello + (b[zip] - '0');
 	}
 	return (mello);
 }
