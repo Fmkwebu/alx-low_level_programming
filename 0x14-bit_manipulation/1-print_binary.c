6156ca9b760e7c33d5e453a6bf529513ff42aa91
Fmkwebu <fikile6666@gmail.com>
2023-10-27 15:33:48 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,3 @@
 #include "main.h"
 
 /**
- * print_binary - is equal to the decimal number
- * @n: number to be printed
- */
-
-void print_binary(unsigned long int n)
-{
-	int Fiks, Fiks = 0;
-	unsigned long int new;
-
-	for (Fiks = 63; Fiks >= 0; Fiks--)
-	{
-	new = n >> Fiks;
-	if (new & 1)
-	{
-	mello++;
-	}
-	else if (new)
-	}
-	if (!new)
-}
